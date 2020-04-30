8e5ea226a2c3b08348d44fc1118753fd2040974b
Christopher Bell <hexstudiosteam@gmail.com>
2020-04-30 15:23:01 -0500

    Added side navigation, made it a little more mobile friendly, and added a welcome page

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-
 $pageName .= 'pages/' . $_GET["page"] . '.php';
-
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +7,7 @@ $pageName .= 'pages/' . $_GET["page"] . '.php';
     <head>
         <title>Wiki</title>
         <link rel="stylesheet" href="css/master.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <h1>Hex Studios Wiki</h1>
@@ -17,6 +16,7 @@ $pageName .= 'pages/' . $_GET["page"] . '.php';
         <div id="sidenav">
             <b>Navigation</b>
             <ul>
+                <li><a href="index.php?page=welcome">Welcome</a></li>
                 <li><a href="index.php?page=test">Test page</a></li>
                 <li><a href="index.php?page=test2">Test page 2</a></li>
             </ul>
---
+++ pages/welcome.php
@@ -0,0 +1,4 @@
+<div>
+    <h2>Welcome to the Hex Studios Wiki</h2>
+    <p>This wiki was developed by Hex Studios and is licensed under GPL-3.0. </p>
+</div>
